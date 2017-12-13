f85557150661ed98fe9275bda55852c5a4e966e7
kaxel <ka@ashland.io>
2017-12-13 11:32:53 -0800

    wondering how to handle raising errors- iam using a string maybe I need to design an error class

--- lib/Calc.rb
+++ lib/Calc.rb
@@ -1,7 +1,35 @@
 class Calc
   
-  def take(arg)
-    return arg
+  OPERATORS = ["+", "-", "/", "*"]
+  
+  def take(args)
+    all_args = []
+    my_args = args.split(" ")
+    last_arg = ""
+    my_args.each do |a|
+      #puts "process #{a}"
+      last_arg = a
+      all_args.push last_arg
+    end
+    if OPERATORS.include?(last_arg)
+      #operate
+      #check for num of args
+      if all_args.size<3
+        all_args.pop
+        return "ERROR"
+      else
+        all_args.pop
+        first_val = all_args.pop
+        second_val = all_args.pop
+        new_val = eval("#{first_val} #{last_arg} #{second_val}")
+        all_args << new_val
+        return new_val
+      end
+      
+    else
+      return last_arg.to_i
+    end
+    
   end
   
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -4,7 +4,27 @@ RSpec.describe Calc, "#inputs" do
   context "on loading" do
     it "returns a single input" do
       calc = Calc.new
-      expect calc.take(1) == 1
+      expect(calc.take("1")).to eq(1)
+    end
+    
+    it "returns the last of two or more subsequent inputs" do
+      calc = Calc.new
+      expect(calc.take("1 3")).to eq(3)
+      expect(calc.take("1 3 7 7 2")).to eq(2) 
+      expect(calc.take("1 3 44 66 9999")).to eq(9999) 
+    end
+  end
+  
+  context " with action" do
+    it "uses operators correctly" do
+      calc = Calc.new
+      expect(calc.take("1 3 +")).to eq(4)
+      expect(calc.take("9 4 66 13 -")).to eq(-53)
+    end
+    
+    it "errors when operator cannot find enough inputs" do
+      calc = Calc.new
+      expect(calc.take("3 +")).to eq("ERROR")
     end
   end
 end
